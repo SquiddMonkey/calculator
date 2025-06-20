a780d85e9db79443bac2a6a68d7ef209277314e9
Jacob Buck <123811017+SquidMonkey16@users.noreply.github.com>
2025-06-20 03:35:36 -0700

    Create main layout

--- main.js
+++ main.js
@@ -1,3 +1,100 @@
+const answerBox = document.querySelector(".answer-box");
+const buttons = document.querySelectorAll(".buttons button");
+const pressedKeys = {};
+
+let input;
+let decimalAvailable = true;
+input = answerBox.textContent;
+
+// button1 = document.querySelector("data-button='" + 1 + "']");
+
+buttons.forEach(button => {
+    let char = convertButtonTextToKey(button.textContent);
+
+    button.addEventListener("click", () => handleInput(char));
+});
+
+document.addEventListener("keydown", (event) => {
+    const key = event.key;
+    pressedKeys[key] = true;
+
+    console.log("Key: " + key); // delete
+
+    handleInput(key);
+})
+
+document.addEventListener("keyup", (event) => {
+    delete pressedKeys[event.key];
+});
+
+function convertButtonTextToKey(buttonText) {
+    if (buttonText === "DEL")
+        return "Backspace";
+    else if (buttonText === "AC")
+        return "Meta + Backspace";
+    else
+        return buttonText;
+}
+
+function metaPressed() {
+    return pressedKeys.Meta === true;
+}
+
+function handleInput(char) {
+    if (isValidInput(char)) {
+        appendInput(char);
+        scrollFullyLeft(answerBox);
+    }
+}
+
+function isValidInput(char) {
+    if (char >= 0 && char < 10 || 
+        char === "+" ||
+        char === "-" ||
+        char === "*" ||
+        char === "/" ||
+        char === "." ||
+        char === "Backspace" ||
+        char === "=" ||
+        char === "Enter") {
+        return true;
+    }
+    return false;
+}
+
+function appendInput(char) {
+    if (isDigit(char)) {
+        if (input === "0") {
+            input = "";
+        }
+        input += char;
+    }
+
+    if ((char === "+" || char === "-" || char === "*" || char === "/") && lastCharWasDigit()) {
+        decimalAvailable = true;
+        input += " " + char + " ";
+    }
+
+    if (char === "." && decimalAvailable) {
+        decimalAvailable = false;
+        input += char;
+    }
+
+    answerBox.textContent = input;
+}
+
+function isDigit(char) {
+    return char >= 0 && char < 10;
+}
+
+function lastCharWasDigit() {
+    return isDigit(input.at(-1));
+}
+
+function scrollFullyLeft(element) {
+    element.scrollLeft += element.scrollWidth;
+}
+
 function add(a, b) {
     return a + b;
 }
